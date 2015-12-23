a3e4892951922d6b1dcae82ec4e4b798081d2b28
Bingcai Liu <liu.3444.osu@gmail.com>
2015-12-23 14:35:56 -0500

    adding models

--- Test_NP.R
+++ Test_NP.R
@@ -1,3 +1,5 @@
+setwd("C:/Users/Bingcai/OSU/AED Research/Mississipi/2015 Summer_All sites/Nitrogen Price data")
+
 NP = read.csv("NP_csv.csv")
 dc_date_sq = with(NP, dc_date^2)
 dc_date_qt = with(NP, dc_date^3)
@@ -7,3 +9,5 @@ NP.3 = with(NP, lm(Price_Modi~ dc_date_qt + dc_date_sq + dc_date))
 summary(NP.1)
 summary(NP.2)
 summary(NP.3)
+
+with(NP, plot(Period, Price_Modi))
